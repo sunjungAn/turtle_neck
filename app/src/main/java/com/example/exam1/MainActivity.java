c314cb619ab179727dac3444580b56369ccd8bb3
KkiYya <sja3410@gmail.com>
2020-04-10 12:52:01 +0900

    init commit

---
+++ app/src/main/java/com/example/exam1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.exam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/exam1/SeconActivity.java
@@ -0,0 +1,4 @@
+package com.example.exam1;
+
+public class SeconActivity {
+}
