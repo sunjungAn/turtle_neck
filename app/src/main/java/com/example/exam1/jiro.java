79efe961729fc02a3b707c4f678c77fc87b4a030
KkiYya <sja3410@gmail.com>
2020-04-17 13:31:18 +0900

    화면 밝기

--- app/src/main/java/com/example/exam1/jiro.java
+++ app/src/main/java/com/example/exam1/jiro.java
@@ -2,31 +2,35 @@ package com.example.exam1;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class jiro extends AppCompatActivity implements SensorEventListener { //SensorEventListener를 사용한다.
+public class jiro extends AppCompatActivity implements SensorEventListener{ //SensorEventListener를 사용한다.
 
     Button toast_btn;
     TextView textView;
 
     private SensorManager sensorManager; //객체 생성
     private final float[] accelerometerReading = new float[3]; //가속도
-    private final float[] magnetometerReading = new float[3]; //
+    private final float[] magnetometerReading = new float[3];
+    private WindowManager.LayoutParams params;
+    private float brightness;
+    //
 
 
     private final float[] rotationMatrix = new float[9];  //회전값
     private final float[] orientationAngles = new float[3];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,8 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
         setContentView(R.layout.activity_main);
         toast_btn = findViewById(R.id.toast_btn);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        params = getWindow().getAttributes();
+
 
         toast_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +48,8 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
                 toast1.show();
             }
         });
+
+
     }
 
     @Override
@@ -49,10 +57,10 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
         // Do something here if sensor accuracy changes.
         // You must implement this callback in your code.
     }
+
     @Override
     protected void onResume() { //아직 사용X
         super.onResume();
-
         // Get updates from the accelerometer and magnetometer at a constant rate.
         // To make batch operations more efficient and reduce power consumption,
         // provide support for delaying updates to the application.
@@ -75,6 +83,7 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
     protected void onPause() { //아직 사용X
         super.onPause();
 
+
         // Don't receive any more updates from either sensor.
         sensorManager.unregisterListener(this);
     }
@@ -82,7 +91,9 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
     // Get readings from accelerometer and magnetometer. To simplify calculations,
     // consider storing these readings as unit vectors.
     @Override
-    public void onSensorChanged(SensorEvent event) { //각도가 움직일때 마다 각을 바꿔준다.
+    public void onSensorChanged(SensorEvent event) {
+        Intent intent = new Intent(getApplicationContext(), MyService.class);
+        startService(intent);//각도가 움직일때 마다 각을 바꿔준다.
         float x = event.values[0];
         float y = event.values[1];
         float z = event.values[2];
@@ -99,7 +110,9 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             textView =  findViewById(R.id.textView);
             textView.setText("value_x"+anglexy+"\nvalue_y"+anglexz+"\nvalue_z"+ angleyz);
-
+            if(angleyz <= 50)
+                changeScreenBrightness(80);
+            else changeScreenBrightness(-1);
         }
     }
 
@@ -116,7 +129,14 @@ public class jiro extends AppCompatActivity implements SensorEventListener { //S
 
         // "mOrientationAngles" now has up-to-date information.
     }
+
 */
+  private void changeScreenBrightness(int value) {
+      Window window = getWindow();
+      WindowManager.LayoutParams layoutParams = window.getAttributes();
+      layoutParams.screenBrightness = value * 1.0f/255;
+      window.setAttributes(layoutParams);
+  }
 }
 
 
