adb07ba3e80588e8bdaf9c03cc765a9dcd5d7d53
nayeone <ice1011@naver.com>
2020-05-18 13:23:27 +0900

    xml 변경

---
+++ app/src/main/java/com/example/exam1/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.a20200515_real;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    //프래그먼트 교체를 보여주는 처리를 구현한 곳
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return FragMonday.newInstance();
+            case 1:
+                return FragTuesday.newInstance();
+            case 2:
+                return FragWednesday.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    //상단의 탭 레이아웃 인디케이터 쪽에 텍스트를 선언하는 곳
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "스트레칭1";
+            case 1:
+                return "스트레칭2";
+            case 2:
+                return "스트레칭3";
+            default:
+                return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/exam1/popup.java
@@ -0,0 +1,36 @@
+package com.example.a20200515_real;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class popup extends AppCompatActivity {
+
+    private FragmentPagerAdapter fragmentPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+
+        DisplayMetrics dm  = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8), (int)(height*.6));
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
