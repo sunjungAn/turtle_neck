807e11a2220911d59422c18b0cba63429e0c292b
nayeone <ice1011@naver.com>
2020-05-25 16:08:39 +0900

    frag부분 3-1 3-2 내용 생성

--- app/src/main/java/com/example/exam1/Fragment_stretching3_2.java
+++ app/src/main/java/com/example/exam1/Fragment_stretching3_2.java
@@ -1,14 +1,27 @@
 package com.example.exam1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Fragment_stretching3_2 extends AppCompatActivity {
+public class Fragment_stretching3_2 extends Fragment{
+    private View view;
 
+    public static Fragment_stretching3_2 newInstance(){
+        Fragment_stretching3_2 fragment_Stretching3_2 = new Fragment_stretching3_2();
+        return fragment_Stretching3_2;
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_stretching3_2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_fragment_stretching3_2,container,false);
+        return view;
     }
 }
